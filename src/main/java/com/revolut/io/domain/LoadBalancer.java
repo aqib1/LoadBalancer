d909d2700a169e76010efaf07b1aedda17d11d58
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-14 02:20:15 +0100

    Update LoadBalancer.java

--- src/main/java/com/revolut/io/domain/LoadBalancer.java
+++ src/main/java/com/revolut/io/domain/LoadBalancer.java
@@ -1,27 +1,34 @@
 package com.revolut.io.domain;
 
 import com.revolut.io.domain.model.BackendInstance;
+import com.revolut.io.domain.strategies.SelectionStrategy;
+import com.revolut.io.domain.strategies.impl.RandomSelectionStrategy;
 import com.revolut.io.exceptions.EmptyLoadBalancerException;
 import com.revolut.io.exceptions.LoadBalancerOverflowException;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class LoadBalancer {
     private static final int CAPACITY = 10;
     private final int capacity;
     private final Map<String, BackendInstance> instances;
+    private final SelectionStrategy selectionStrategy;
 
     public LoadBalancer() {
-        this(CAPACITY);
+        this(CAPACITY, new RandomSelectionStrategy());
     }
 
-    public LoadBalancer(int capacity) {
+    public LoadBalancer(int capacity, SelectionStrategy selectionStrategy) {
         this.capacity = capacity;
         this.instances = new ConcurrentHashMap<>();
+        this.selectionStrategy = selectionStrategy;
     }
 
     public boolean addInstance(BackendInstance backendInstance) {
+        Objects.requireNonNull(backendInstance);
+
         if (isLoadBalancerOverflow())
             throw new LoadBalancerOverflowException("LoadBalancer does not have more capacity");
 
@@ -31,11 +38,23 @@ public class LoadBalancer {
         ) == null;
     }
 
+    public BackendInstance selectInstance() {
+        if(isLoadBalancerEmpty())
+            throw new EmptyLoadBalancerException("Load balancer is empty");
+
+        var instances = this.instances.keySet().stream().toList();
+        return this.instances.get(
+                this.selectionStrategy.selectInstance(instances)
+        );
+    }
+
     private boolean isLoadBalancerOverflow() {
         return instances.size() == this.capacity;
     }
 
     public boolean removeInstance(BackendInstance backendInstance) {
+        Objects.requireNonNull(backendInstance);
+
         if(isLoadBalancerEmpty())
             throw new EmptyLoadBalancerException("Load balancer is empty");
 
