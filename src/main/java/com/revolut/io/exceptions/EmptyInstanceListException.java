922263e2314279e710867161155eb7929c39b136
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-14 02:19:51 +0100

    Create EmptyInstanceListException.java

---
+++ src/main/java/com/revolut/io/exceptions/EmptyInstanceListException.java
@@ -0,0 +1,7 @@
+package com.revolut.io.exceptions;
+
+public class EmptyInstanceListException extends RuntimeException {
+    public EmptyInstanceListException(String message) {
+        super(message);
+    }
+}
