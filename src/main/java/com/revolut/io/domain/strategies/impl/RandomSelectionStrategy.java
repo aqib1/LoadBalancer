50a868174c0cc9157ea668ddffec6cbb59d6d9d2
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-15 04:48:36 +0100

    Update RandomSelectionStrategy.java

--- src/main/java/com/revolut/io/domain/strategies/impl/RandomSelectionStrategy.java
+++ src/main/java/com/revolut/io/domain/strategies/impl/RandomSelectionStrategy.java
@@ -1,19 +1,16 @@
-package com.revolut.io.domain.strategies.impl;
+package com.revolut.io.strategies.impl;
 
-import com.revolut.io.domain.strategies.SelectionStrategy;
+import com.revolut.io.model.BackendInstance;
+import com.revolut.io.strategies.SelectionStrategy;
 
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomSelectionStrategy implements SelectionStrategy {
-
     @Override
-    public String selectInstance(List<String> instances) {
-        validateInstances(instances);
+    public BackendInstance selectInstance(List<BackendInstance> instances) {
         return instances.get(
-                ThreadLocalRandom.current().nextInt(
-                        instances.size()
-                )
+                ThreadLocalRandom.current().nextInt(instances.size())
         );
     }
 }
