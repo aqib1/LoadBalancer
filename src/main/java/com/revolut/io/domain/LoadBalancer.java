0c48cf443d595918ac6c9c24fc83d8d99253ec85
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-15 04:50:12 +0100

    Update LoadBalancer.java

--- src/main/java/com/revolut/io/domain/LoadBalancer.java
+++ src/main/java/com/revolut/io/domain/LoadBalancer.java
@@ -1,69 +1,55 @@
-package com.revolut.io.domain;
+package com.revolut.io;
 
-import com.revolut.io.domain.model.BackendInstance;
-import com.revolut.io.domain.strategies.SelectionStrategy;
-import com.revolut.io.domain.strategies.impl.RandomSelectionStrategy;
 import com.revolut.io.exceptions.EmptyLoadBalancerException;
 import com.revolut.io.exceptions.LoadBalancerOverflowException;
+import com.revolut.io.model.BackendInstance;
+import com.revolut.io.strategies.SelectionStrategy;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class LoadBalancer {
     private static final int CAPACITY = 10;
-    private final int capacity;
-    private final Map<String, BackendInstance> instances;
     private final SelectionStrategy selectionStrategy;
+    private final Map<String, BackendInstance> instances;
 
-    public LoadBalancer() {
-        this(CAPACITY, new RandomSelectionStrategy());
-    }
-
-    public LoadBalancer(int capacity, SelectionStrategy selectionStrategy) {
-        this.capacity = capacity;
-        this.instances = new ConcurrentHashMap<>();
+    public LoadBalancer(SelectionStrategy selectionStrategy) {
         this.selectionStrategy = selectionStrategy;
+        this.instances = new ConcurrentHashMap<>();
     }
 
-    public boolean addInstance(BackendInstance backendInstance) {
-        Objects.requireNonNull(backendInstance);
+    public boolean addInstance(BackendInstance instance) {
+        checkLoadBalancerOverflow();
 
-        if (isLoadBalancerOverflow())
-            throw new LoadBalancerOverflowException("LoadBalancer does not have more capacity");
-
-        return this.instances.putIfAbsent(
-                backendInstance.address(),
-                backendInstance
+        return instances.putIfAbsent(
+                instance.address(),
+                instance
         ) == null;
     }
 
-    public BackendInstance selectInstance() {
-        if(isLoadBalancerEmpty())
-            throw new EmptyLoadBalancerException("Load balancer is empty");
-
-        var instances = this.instances.keySet().stream().toList();
-        return this.instances.get(
-                this.selectionStrategy.selectInstance(instances)
-        );
-    }
-
-    private boolean isLoadBalancerOverflow() {
-        return instances.size() == this.capacity;
-    }
-
-    public boolean removeInstance(BackendInstance backendInstance) {
-        Objects.requireNonNull(backendInstance);
-
-        if(isLoadBalancerEmpty())
-            throw new EmptyLoadBalancerException("Load balancer is empty");
+    public boolean removeInstance(BackendInstance instance) {
+        checkLoadBalancerIsEmpty();
 
         return instances.remove(
-                backendInstance.address()
+                instance.address()
         ) != null;
     }
 
-    private boolean isLoadBalancerEmpty() {
-        return this.instances.isEmpty();
+    public BackendInstance selectInstance() {
+        checkLoadBalancerIsEmpty();
+
+        return this.selectionStrategy.selectInstance(
+                instances.values().stream().toList()
+        );
+    }
+
+    private void checkLoadBalancerIsEmpty() {
+        if(instances.isEmpty())
+            throw new EmptyLoadBalancerException("Load balancer is empty");
+    }
+
+    private void checkLoadBalancerOverflow() {
+         if(this.instances.size() == CAPACITY)
+             throw new LoadBalancerOverflowException("Load balancer is overflowed");
     }
 }
