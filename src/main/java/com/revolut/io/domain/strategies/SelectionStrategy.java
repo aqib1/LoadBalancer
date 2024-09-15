21f7d42ba0806215929d89dc7d7afa6ac5f9bc08
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-15 04:48:11 +0100

    Update SelectionStrategy.java

--- src/main/java/com/revolut/io/domain/strategies/SelectionStrategy.java
+++ src/main/java/com/revolut/io/domain/strategies/SelectionStrategy.java
@@ -1,16 +1,9 @@
-package com.revolut.io.domain.strategies;
+package com.revolut.io.strategies;
 
-import com.revolut.io.exceptions.EmptyInstanceListException;
+import com.revolut.io.model.BackendInstance;
 
 import java.util.List;
-import java.util.Objects;
 
 public interface SelectionStrategy {
-    String selectInstance(List<String> instances);
-
-    default void validateInstances(List<String> instances) {
-        Objects.requireNonNull(instances);
-        if(instances.isEmpty())
-            throw new EmptyInstanceListException("Empty instance list not allowed");
-    }
+    BackendInstance selectInstance(List<BackendInstance> instances);
 }
