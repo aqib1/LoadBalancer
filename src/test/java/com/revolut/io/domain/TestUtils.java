c70bb20fba1ed0127b79c95cbc757a525ae60755
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-15 04:50:55 +0100

    Update TestUtils.java

--- src/test/java/com/revolut/io/domain/TestUtils.java
+++ src/test/java/com/revolut/io/domain/TestUtils.java
@@ -1,31 +1,33 @@
-package com.revolut.io.domain;
+package com.revolut.io;
 
-import com.revolut.io.exceptions.EmptyInstanceListException;
-import org.junit.jupiter.params.provider.Arguments;
+import com.revolut.io.model.BackendInstance;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestUtils {
-
-    public static final List<String> BACKEND_INSTANCES = List.of(
-            "127.0.0.1",
-            "127.0.0.2",
-            "127.0.0.3"
+    public static final List<BackendInstance> INSTANCES = List.of(
+            new BackendInstance("127.0.0.1", 4),
+            new BackendInstance("127.0.0.2", 3),
+            new BackendInstance("127.0.0.3", 2)
     );
 
-    public static final List<String> BACKEND_ROUND_ROBIN_INSTANCES = Stream.concat(
-            BACKEND_INSTANCES.stream(),
-            BACKEND_INSTANCES.subList(0, BACKEND_INSTANCES.size() - 1).stream()
-    ).collect(Collectors.toList());
+    public static final List<BackendInstance> WEIGHTED_ROUND_ROBIN_INSTANCE = List.of(
+            new BackendInstance("127.0.0.1", 4),
+            new BackendInstance("127.0.0.1", 4),
+            new BackendInstance("127.0.0.2", 3),
+            new BackendInstance("127.0.0.1", 4),
+            new BackendInstance("127.0.0.2", 3),
+            new BackendInstance("127.0.0.3", 2),
+            new BackendInstance("127.0.0.1", 4),
+            new BackendInstance("127.0.0.2", 3),
+            new BackendInstance("127.0.0.3", 2)
+    );
 
-    public static Stream<Arguments> invalidSelectInstanceParams() {
-        return Stream.of(
-                Arguments.of(null, NullPointerException.class),
-                Arguments.of(List.of(), EmptyInstanceListException.class)
-        );
-    }
+    public static final List<BackendInstance> ROUND_ROBIN_INSTANCE = Stream.concat(
+            INSTANCES.stream(), INSTANCES.stream()
+    ).collect(Collectors.toList());
 
     private TestUtils() {
 
