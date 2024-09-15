e7fef52d672a5e024e0b0998ca03437a2c9b2562
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-15 04:49:17 +0100

    Update RoundRobinSelectionStrategy.java

--- src/main/java/com/revolut/io/domain/strategies/impl/RoundRobinSelectionStrategy.java
+++ src/main/java/com/revolut/io/domain/strategies/impl/RoundRobinSelectionStrategy.java
@@ -1,21 +1,20 @@
-package com.revolut.io.domain.strategies.impl;
+package com.revolut.io.strategies.impl;
 
-import com.revolut.io.domain.strategies.SelectionStrategy;
+import com.revolut.io.model.BackendInstance;
+import com.revolut.io.strategies.SelectionStrategy;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class RoundRobinSelectionStrategy implements SelectionStrategy {
     private final AtomicInteger index;
-
     public RoundRobinSelectionStrategy() {
         this.index = new AtomicInteger(0);
     }
-
     @Override
-    public String selectInstance(List<String> instances) {
+    public BackendInstance selectInstance(List<BackendInstance> instances) {
         return instances.get(
-                this.index.getAndUpdate(i -> (i + 1) % instances.size())
+                index.getAndUpdate(i -> (i + 1) % instances.size())
         );
     }
 }
